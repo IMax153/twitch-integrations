d3cb2e2081b1f9d3bc15fd6aa2daae0eefa44e30
Maxwell Brown <maxwellbrown1990@gmail.com>
2025-06-26 12:01:02 -0400

    add support for song-queue chat macro

--- src/services/chat-macros.ts
+++ src/services/chat-macros.ts
@@ -1,6 +1,8 @@
-import { Array, Config, Effect, Exit, Match, Option, Schedule, Stream } from "effect"
+import { Array, Config, Effect, Exit, Match, Option, pipe, Schedule, Stream } from "effect"
+import type * as Api from "../domain/spotify.js"
 import type { ChannelChatMessageEvent } from "../domain/twitch.js"
 import { TwitchClient } from "../twitch/client.js"
+import type { SongQueue } from "./song-queue.js"
 import { SpotifySongQueue } from "./song-queue.js"
 
 export class TwitchChatMacros extends Effect.Service<TwitchChatMacros>()("app/TwitchChatMacros", {
@@ -9,6 +11,19 @@ export class TwitchChatMacros extends Effect.Service<TwitchChatMacros>()("app/Tw
 
     const spotifySongQueue = yield* SpotifySongQueue
     const twitchClient = yield* TwitchClient
+
+    const handleEmptyQueue = Effect.fn("TwitchChatMacros.handleEmptyQueue")(
+      function*(event: ChannelChatMessageEvent) {
+        const message = `@${event.chatter_user_name} no songs are currently in the Spotify song queue`
+        return yield* twitchClient.sendChatMessage(message).pipe(
+          Effect.retry({
+            times: 3,
+            schedule: Schedule.fixed("200 millis")
+          }),
+          Effect.ignoreLogged
+        )
+      }
+    )
 
     const getCurrentSong = Effect.fn("TwitchChatMacros.getCurrentSong")(
       function*(event: ChannelChatMessageEvent) {
@@ -25,21 +40,11 @@ export class TwitchChatMacros extends Effect.Service<TwitchChatMacros>()("app/Tw
         }
         const queueItem = Array.head(exit.value)
         if (Option.isNone(queueItem) || queueItem.value.type === "episode") {
-          const message = `@${event.chatter_user_name} no songs are currently in the Spotify song queue`
-          return yield* twitchClient.sendChatMessage(message).pipe(
-            Effect.retry({
-              times: 3,
-              schedule: Schedule.fixed("200 millis")
-            }),
-            Effect.ignoreLogged
-          )
+          return yield* handleEmptyQueue(event)
         }
         if (queueItem.value.type === "track") {
-          const song = queueItem.value
-          const name = song.name ?? "<unknown>"
-          const artists = Array.filterMap(song.artists ?? [], (artist) => Option.fromNullable(artist.name))
-          const artistsStr = artists.length === 0 ? "" : `by ${artists.join(", ")}`
-          const message = `Current Song: ${name}${artistsStr}`
+          const track = renderTrack(queueItem.value)
+          const message = `Current Song: ${track}`
           return yield* twitchClient.sendChatMessage(message).pipe(
             Effect.retry({
               times: 3,
@@ -51,9 +56,45 @@ export class TwitchChatMacros extends Effect.Service<TwitchChatMacros>()("app/Tw
       }
     )
 
-    // !queue
+    const getSongQueue = Effect.fn("TwitchChatMacros.getSongQueue")(
+      function*(event: ChannelChatMessageEvent) {
+        const exit = yield* Effect.exit(spotifySongQueue.getQueue())
+        if (Exit.isFailure(exit)) {
+          const message = `@${event.chatter_user_name} sorry, your request for the song queue failed! NotLikeThis`
+          return yield* twitchClient.sendChatMessage(message).pipe(
+            Effect.retry({
+              times: 3,
+              schedule: Schedule.fixed("200 millis")
+            }),
+            Effect.ignoreLogged
+          )
+        }
+        const tracks = Array.take(Array.filter(exit.value, isTrackObject), 4)
+        if (Array.isNonEmptyReadonlyArray(tracks)) {
+          const current = renderTrack(Array.headNonEmpty(tracks))
+          const currentMsg = `Currently Playing: ${current}`
+          const next = pipe(
+            Array.drop(tracks, 1),
+            Array.map((track, index) => `${index + 1}. ${renderTrack(track)}`),
+            Array.join(" | ")
+          )
+          const nextMsg = next.length > 0 ? ` ||| Next Up: ${next}` : ""
+          const message = `${currentMsg}${nextMsg}`
+          return yield* twitchClient.sendChatMessage(message).pipe(
+            Effect.retry({
+              times: 3,
+              schedule: Schedule.fixed("200 millis")
+            }),
+            Effect.ignoreLogged
+          )
+        }
+        return yield* handleEmptyQueue(event)
+      }
+    )
+
     const handleChatMacro = Match.type<ChannelChatMessageEvent>().pipe(
       Match.when({ message: { text: "!song" } }, (event) => getCurrentSong(event)),
+      Match.when({ message: { text: "!song-queue" } }, (event) => getSongQueue(event)),
       Match.orElse(() => Effect.void)
     )
 
@@ -85,3 +126,14 @@ export class TwitchChatMacros extends Effect.Service<TwitchChatMacros>()("app/Tw
     SpotifySongQueue.Default
   ]
 }) {}
+
+function isTrackObject(item: SongQueue[number]): item is Api.TrackObject {
+  return item.type === "track"
+}
+
+function renderTrack(track: Api.TrackObject) {
+  const name = track.name ?? "<unknown>"
+  const artists = Array.filterMap(track.artists ?? [], (artist) => Option.fromNullable(artist.name))
+  const artistsStr = artists.length === 0 ? "" : ` by ${artists.join(", ")}`
+  return `${name}${artistsStr}`
+}
